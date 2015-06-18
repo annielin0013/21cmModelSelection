d0fb75a39d1c4c3752c7d9527e040f97fe9e3ae0
Annie Lin <amadeuslin@berkeley.edu>
2015-06-18 15:29:27 -0700

    Fixed oscillation problem

---
+++ fourier1D.py
@@ -0,0 +1,17 @@
+from numpy.fft import fft, fftshift, ifftshift
+from numpy import array, arange, concatenate
+from math import sqrt, pi, exp
+from matplotlib import pyplot
+
+def gaussian(x, sigma):
+    return (1/(sqrt(2*pi*pow(sigma, 2))))*exp(-pow(x, 2)/(2*pow(sigma, 2)))
+
+x_axis = arange(-100, 100, .1)
+
+y_axis1 = array([gaussian(i, 1) for i in x_axis])
+
+y_axis2 = ifftshift(fft(fftshift(y_axis1)))
+
+pyplot.plot(x_axis, y_axis1)
+pyplot.plot(x_axis, y_axis2)
+pyplot.show()
